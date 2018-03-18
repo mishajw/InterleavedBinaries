baa72a3026d41942ea3bb4f2bd42611d1c281812
Misha Wagner <mishajw@gmail.com>
2018-03-18 18:17:13 +0000

    Reorganise Lib.hs to have visible functions for building programs from C and ASM files

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,11 +2,13 @@ module Lib (
   interleavePrograms,
   interleaveCFiles,
   interleaveAsmFiles,
+  cFileToProgram,
+  asmFileToProgram,
   saveProgram)
   where
 
 import AsmTransformer (transform)
-import System.FilePath.Posix ((</>), (<.>))
+import System.FilePath.Posix ((</>), (<.>), dropExtension, takeFileName)
 import System.IO.Temp (withSystemTempDirectory)
 import System.Process (readProcessWithExitCode)
 import qualified Asm
@@ -25,13 +27,9 @@ interleaveCFiles
   :: FilePath -- | The path of the first .c file
   -> FilePath -- | The path of the second .c file
   -> IO Asm.Program -- | The interleaved program
-interleaveCFiles path1 path2 = withSystemTempDirectory "/tmp/" run where
+interleaveCFiles path1 path2 =
   -- | Run on @cFileToAsmFile@ both paths and pass to @interleavePrograms@
-  run :: FilePath -> IO Asm.Program
-  run directory = do
-    asmFile1 <- cFileToAsmFile path1 directory 1
-    asmFile2 <- cFileToAsmFile path2 directory 2
-    interleaveAsmFiles asmFile1 asmFile2
+    interleavePrograms <$> cFileToProgram path1 <*> cFileToProgram path2
 
 -- | Interleave two ASM (.s) files
 -- Delagates to @interleavePrograms@
@@ -41,6 +39,31 @@ interleaveAsmFiles
   -> IO Asm.Program -- ^ the interleaved program
 interleaveAsmFiles path1 path2 =
   interleavePrograms <$> asmFileToProgram path1 <*> asmFileToProgram path2
+
+-- | Load an @Asm.Program@ from a `.c` file
+cFileToProgram
+  :: FilePath -- ^ The path of the `.c` file
+  -> IO Asm.Program -- ^ The program read from the file
+cFileToProgram cPath = withSystemTempDirectory "/tmp/" run where
+  run :: FilePath -> IO Asm.Program
+  run directory = do
+    -- Create path for ASM file
+    let fileName = dropExtension $ takeFileName cPath
+    let asmPath = directory </> fileName <.> "a"
+    -- Create the ASM file
+    cFileToAsmFile cPath asmPath
+    -- Create a program from the ASM file
+    asmFileToProgram asmPath
+
+-- | Convert an ASM file to a @Asm.Program@
+asmFileToProgram
+  :: FilePath -- ^ The path of the ASM file
+  -> IO Asm.Program -- ^ The program
+asmFileToProgram inputPath = do
+  contents <- readFile inputPath
+  case Asm.stringToProgram contents of
+    Left e -> error $ "Error trying to parse ASM: " ++ e
+    Right program -> return program
 
 -- | Save a program to an ASM file
 saveProgram
@@ -52,29 +75,18 @@ saveProgram path program = writeFile path $ Asm.programToString program
 -- | Change a .c file to an ASM (.s) file
 cFileToAsmFile
   :: FilePath -- ^ The path of the .c file
-  -> FilePath -- ^ The output path for the ASM file
-  -> Int -- ^ Index of the program we're creating
-  -> IO FilePath -- ^ The path of the created ASM file
-cFileToAsmFile inputPath directory programIndex = do
-  let asmFile = directory </> ("asm" ++ show programIndex) <.> "s"
+  -> FilePath -- ^ The output file path for the ASM file
+  -> IO ()
+cFileToAsmFile inputPath outputPath = do
+  -- let asmFile = directory </> ("asm" ++ show programIndex) <.> "s"
   (exitCode, _, _) <- readProcessWithExitCode "gcc" [
     "-O0",
     "-S", inputPath,
-    "-o", asmFile] ""
+    "-o", outputPath] ""
 
   case exitCode of
     Exit.ExitSuccess ->
-      return asmFile
+      return ()
     Exit.ExitFailure n ->
       error $ "Bad return code when generating ASM: " ++ show n
-
--- | Convert an ASM file to a @Asm.Program@
-asmFileToProgram
-  :: FilePath -- ^ The path of the ASM file
-  -> IO Asm.Program -- ^ The program
-asmFileToProgram inputPath = do
-  contents <- readFile inputPath
-  case Asm.stringToProgram contents of
-    Left e -> error $ "Error trying to parse ASM: " ++ e
-    Right program -> return program
 
