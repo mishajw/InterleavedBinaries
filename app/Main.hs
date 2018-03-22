51321c218120a65daf7a0cbaf800d6ed6d054e7a
Misha Wagner <mishajw@gmail.com>
2018-03-22 17:56:53 +0000

    Add simple command line argument parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,23 @@
 module Main where
 
 import Lib
+import System.Environment (getArgs, getProgName)
+import Data.List (isInfixOf)
+
+outputFilePath = "bin/output.s"
 
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+  progName <- getProgName
+
+  program <- case args of
+    [path1, path2] | ".c" `isInfixOf` path1 && ".c" `isInfixOf` path2 ->
+      interleaveCFiles path1 path2
+    [path1, path2] | ".s" `isInfixOf` path1 && ".s" `isInfixOf` path2 ->
+      interleaveAsmFiles path1 path2
+    _ -> error $ "Usage: " ++ progName ++ " <ASM/C path> <ASM/C path>"
+
+  putStrLn $ "Writing output to " ++ outputFilePath
+  saveProgram outputFilePath program
 
