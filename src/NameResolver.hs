f262b2c940cef77330ce9950adadcdee192046e7
Misha Wagner <mishajw@gmail.com>
2018-03-24 16:55:19 +0000

    Refactor name resolving to work with labels inside arguments

--- src/NameResolver.hs
+++ src/NameResolver.hs
@@ -1,52 +1,48 @@
 module NameResolver (postfixNames)
   where
 
+import Data.Text (pack, unpack, replace)
+import Data.Char (isAlphaNum)
 import Text.Regex (mkRegex, matchRegexAll)
 import qualified Asm
 
 -- | Add some postfix to all names in a programm
 postfixNames :: String -> Asm.Program -> Asm.Program
 postfixNames postfix prog = prog {
-  Asm.funcs = map postfixFunction (Asm.funcs prog),
-  Asm.readOnlyData = map postfixRod (Asm.readOnlyData prog)
+  Asm.funcs = map handleFunc (Asm.funcs prog),
+  Asm.readOnlyData = map handleRod (Asm.readOnlyData prog)
 } where
+
+  allLabels :: [String]
+  allLabels = funcLabels ++ rodLabels where
+    funcLabels = do
+      func <- Asm.funcs prog
+      ins <- Asm.instructions func
+      Asm.labels ins
+    rodLabels = do
+      rod <- Asm.readOnlyData prog
+      ins <- Asm.readOnlyLines rod
+      Asm.labels ins
+
+  handleRenames :: String -> String
+  handleRenames s = foldr f s allLabels where
+    f label s' =
+      unpack $ replace (pack label) (pack (label ++ postfix)) (pack s')
+
   -- | Add postfixes to a function
-  postfixFunction :: Asm.Func -> Asm.Func
-  postfixFunction f = f {
-    Asm.name = Asm.name f ++ postfix,
-    Asm.instructions = map
-                       (postfixLabels . postfixCalls . postfixRodAccess)
-                       (Asm.instructions f)
+  handleFunc :: Asm.Func -> Asm.Func
+  handleFunc f = f {
+    Asm.name = handleRenames $ Asm.name f,
+    Asm.instructions = map handleInstruction (Asm.instructions f)
   }
 
-  -- | Add postfixes to a read only data section
-  postfixRod :: Asm.ReadOnlyData -> Asm.ReadOnlyData
-  postfixRod rod = rod {
-    Asm.readOnlyLines = map postfixLabels (Asm.readOnlyLines rod)
+  handleInstruction :: Asm.Instruction -> Asm.Instruction
+  handleInstruction ins = ins {
+    Asm.arguments = map handleRenames (Asm.arguments ins),
+    Asm.labels = map handleRenames (Asm.labels ins)
   }
 
-  -- | Add postfixes to an instruction's labels
-  postfixLabels :: Asm.Instruction -> Asm.Instruction
-  postfixLabels i = i {
-    Asm.labels = map (++ postfix) (Asm.labels i)
+  handleRod :: Asm.ReadOnlyData -> Asm.ReadOnlyData
+  handleRod rod = rod {
+    Asm.readOnlyLines = map handleInstruction (Asm.readOnlyLines rod)
   }
-
-  -- | Add postfixes to function call label
-  postfixCalls :: Asm.Instruction -> Asm.Instruction
-  postfixCalls i
-    | Asm.command i `elem` ["call", "jmp"] =
-      i {Asm.arguments = map (++ postfix) (Asm.arguments i)}
-    | otherwise = i
-
-  -- | Add postfixes to a read only access
-  postfixRodAccess :: Asm.Instruction -> Asm.Instruction
-  postfixRodAccess i = i {
-    Asm.arguments = map f (Asm.arguments i)
-  } where
-    f arg =
-      let lcRegex = mkRegex "LC[0-9]+" in
-      case matchRegexAll lcRegex arg of
-        Just (before, matched, after, []) ->
-          before ++ matched ++ postfix ++ f after
-        Nothing -> arg
-
